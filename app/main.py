eecd470ab9d1c72236383a5782a16a3d10800cd5
Omkar Parab <omkar211196@gmail.com>
2026-07-07 18:46:25 +0530

    domain updated

--- app/main.py
+++ app/main.py
@@ -21,9 +21,9 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://192.168.1.2:5173",
-        "docmindai-lake.vercel.app"
+        "https://docmindai-lake.vercel.app"
     ],
-    allow_credentials=True,
+    allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
 )
